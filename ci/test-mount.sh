6194062aac621c98c5960277f4e61de72caaf637
bcumming <bcumming@cscs.ch>
2022-07-11 21:58:10 +0200

    wip

--- ci/test-mount.sh
+++ ci/test-mount.sh
@@ -7,11 +7,13 @@
 rm -rf test-dir
 mkdir test-dir
 echo "hello world" >> test-dir/test.txt
-mksquashfs test-dir fs.sqsh
+mksquashfs test-dir fs.sqsh -noappend 
 sudo mkdir /mntpnt
 result=$(squashfs-mount fs.sqsh /mntpnt cat /mntpnt/test.txt)
-if "$result" != "hello world"; then
-    echo "unexpected result: '$result'"
+if [[ "$result" != "hello world" ]]; then
+    echo "------ unexpected result: \"$result\""
     exit 1;
+else
+    echo "------ Success"
 fi
 
